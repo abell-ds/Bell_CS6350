68e56e41a2684a32c81671c09b0f0110881e3dd2
Anna Bell <52009277+abell-ds@users.noreply.github.com>
2023-09-22 02:48:41 +0000

    changed d3.py file method name to main.

--- d3.py
+++ d3.py
@@ -1,17 +1,14 @@
 """
 This file calls my ID3 python implemenation in the ID3 directory.
 """
-import ID3
-from ID3 import *
-import subprocess
-
 import os
 import subprocess
 from urllib.parse import urlparse
 import numpy as np
-from D3 import ID3
+from ID3 import D3 
 
-def d3(data_url: str, data_has_header: bool, max_depth: int = 6, IG_method: str = "", ):
+
+def main(data_url: str, data_has_header: bool, max_depth: int = 6, IG_method: str = ""):
     """
     This is the main method for a user to provide a data url and any specific preferences for running ID3.
     :param data_url: the url to fetch the data
@@ -22,15 +19,17 @@ def d3(data_url: str, data_has_header: bool, max_depth: int = 6, IG_method: str 
     """
 
     #get the data from a url
-    subprocess.run(["wget", data_url])
+    subprocess.run(["wget", data_url], check=False)
     url_path = urlparse(data_url).path
     filename = os.path.basename(url_path)
-    the_tree = None
-
     # Run ID3
     if data_has_header:
         data = np.genfromtxt(filename, delimiter=',', skip_header=1)
-        ID3(data, max_depth, IG_method)
+        return D3.ID3(data, max_depth, IG_method)
     else:
         data = np.genfromtxt(filename, delimiter=',')
-        the_tree = ID3(data, max_depth, IG_method)
+        return D3.ID3(data, max_depth, IG_method)
+    
+def __main__(url):
+    tree = d3(url, True)
+
